7c48e1c59997966dc73d59a5fa00afb7d60c22e2
Jan Dryk <me@uxsoft.cz>
2016-09-27 20:53:03 +0200

    initial

---
+++ uxsoft.Share/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace uxsoft.Share.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
